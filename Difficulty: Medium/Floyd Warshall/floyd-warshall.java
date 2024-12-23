8e2a30e75fd9e7911b09cea069b324cf09e72447
Shubhham yadav <shubhamyadav960212@gmail.com>
2024-12-23 13:31:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+// Initial template for JAVA
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] matrix = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                String[] s = br.readLine().trim().split(" ");
+                for (int j = 0; j < n; j++) matrix[i][j] = Integer.parseInt(s[j]);
+            }
+            Solution obj = new Solution();
+            obj.shortestDistance(matrix);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function template for JAVA
+
+class Solution {
+    public void shortestDistance(int[][] mat) {
+        int [][]dist=mat;
+        int n=mat.length;
+        int m=mat[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==-1){
+                    dist[i][j]=(int)(1e9);
+                }
+            }
+        }
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                dist[i][j]=Math.min(dist[i][j],dist[i][k]+dist[k][j]);
+            }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(dist[i][j]==(int)(1e9)){
+                    mat[i][j]=-1;
+                }else
+                mat[i][j]=dist[i][j];
+            }
+        }
+    }
+}
